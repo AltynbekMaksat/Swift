2196bba2a2bfc20425d529ebfcb7bb45bc583514
AltynbekMaksat <157010910+AltynbekMaksat@users.noreply.github.com>
2024-08-03 19:22:14 +0500

    2лабка

---
+++ lab2.swift
@@ -0,0 +1,50 @@
+//ex1
+    //let friends = 600
+    //
+    //friends = 500 ошибка
+    //
+
+    var friends = 600
+    friends = 500
+    print(friends)
+
+
+//ex2
+    
+    let goalSteps = 10_000
+
+//ex3
+
+    var schooling = 11
+    print(schooling)
+    schooling = 12
+    print(schooling)
+    print("различие между var and let в том что мы не можем изменить константу(let) , а значение var можем менять в любое время")
+
+//ex 4
+    var steps = 0
+    steps = 2000
+    print(steps)
+    print("Хорошая работа! Вы уже на пути к своей ежедневной цели")
+    
+
+//ex 5
+
+    //var name: String
+    //print(name)
+
+    var name : String
+    name = "Maksat"
+    print(name)
+    print("чтобы вывести name мы обязательно должны дать ему значение")
+
+
+// ex 6
+
+//    var distanceTraveled = 0
+//    distanceTraveled = 54.3 ошибка 
+
+    var distanceTraveled : Double = 0
+    distanceTraveled = 54.3
+    print(distanceTraveled)
+
