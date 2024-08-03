82abb3ec9fd4b4afc23e97260ad2e865c065f46f
AltynbekMaksat <157010910+AltynbekMaksat@users.noreply.github.com>
2024-08-04 00:00:27 +0500

    lab5

---
+++ lab5_.swift
@@ -0,0 +1,89 @@
+//ex 1
+
+//var name = "Maksat"
+//print(name)
+//let favoriteQuote = "just do it "
+//print("Моя любимая цитата - \"\(favoriteQuote)\"")
+//
+//let emptyString = ""
+//
+//if emptyString.isEmpty {
+//    print("Здесь ничего нет")
+//} else {
+//    print("Она не пуста, как я думал")
+//}
+
+//ex 2
+
+//
+//let city = "Almaty"
+//let region = "Almatinskaya"
+//
+//let home = city + ", " + region
+//print(home)
+//
+//var introduction = "Я живу в "
+//introduction += home
+//print(introduction)
+//
+//let name: String = "Маksat"
+//let age: Int = 19
+//
+//print("Имя: \(name)")
+//print("Возраст: \(age)")
+//
+//let message = "Меня зовут \(name), и на следующий год мне будет \(age + 1) лет"
+//print(message)
+
+
+//ex 3
+
+//let name: String = "Maksat"
+//let surname: String = "Altynbek"
+//let fullName = name + " " + surname
+//print(fullName)
+//
+//let previousBest: Int = 8_350
+//let newBest: Int = 10_555
+//
+//let congratulations = "Поздравляем, \(fullName)! Вы превзошли свой предыдущий рекорд в \(previousBest) шагов, сделав \(newBest) шагов вчера!"
+//
+//print(congratulations)
+
+//ex 4
+
+//let nameInCaps: String = "MAKSAT"
+//let name: String = "maksat"
+//
+//if nameInCaps == name {
+//    print("Эти две строки равны")
+//} else {
+//    print("Эти две строки не равны")
+//}
+//
+//if nameInCaps.lowercased() == name.lowercased() {
+//    print("\(nameInCaps) и \(name) совпадают")
+//} else {
+//    print("\(nameInCaps) и \(name) не совпадают")
+//}
+//
+//let fullName: String = "Robert Downey Jr."
+//if fullName.hasSuffix("Jr.") {
+//    print("Это имя используется второе поколение")
+//} else {
+//    print("Это имя не используется второе поколение")
+//}
+//
+//let textToSearchThrough = "быть или не быть вот в чём вопрос"
+//
+//let textToSearchFor = "быть или не быть"
+//
+//if textToSearchThrough.lowercased().contains(textToSearchFor.lowercased()) {
+//    print("Я нашел!")
+//} else {
+//    print("Фраза не найдена.")
+//}
+//
+//let name = "Ваше имя"
+//print("Количество символов в имени: \(name.count)")
+//
