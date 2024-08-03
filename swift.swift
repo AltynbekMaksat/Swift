480e3b4d87f2d7650f27784205caa275254f0a8b
AltynbekMaksat <157010910+AltynbekMaksat@users.noreply.github.com>
2024-08-03 17:25:30 +0500

    Create swift.swift

---
+++ swift.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+print("Мой любимый исполнитель : Kaleo ")
+
+print("Любимая песня: Way down we go")
+
+print("Я очень рад что начал изучать Swift")
